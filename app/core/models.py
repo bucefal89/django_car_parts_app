e8007fca6df0e1b2912212a41a2b876f456a8e8a
bucefal89 <stiliyanmitov@gmail.com>
2022-06-10 13:37:40 +0200

    initialize django project

---
+++ app/core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+    )
+    description = models.CharField(
+        max_length=100,
+    )
+
+    
+
